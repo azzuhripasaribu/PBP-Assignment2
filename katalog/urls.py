ff0fbabe95574c83a2265082b3feceb5a485820c
Azzuhri Nauli Pasaribu <azzuhri.nauli@ui.ac.id>
2022-09-10 21:07:04 +0700

    ADDED url routing, views, and html implementation

--- katalog/urls.py
+++ katalog/urls.py
@@ -1,1 +1,6 @@
-# TODO: Implement Routings Here
+from django.urls import path
+from katalog.views import show_katalog
+
+urlpatterns = [
+    path('',show_katalog, name='show_katalog')
+]
--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
+from gettext import Catalog
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_wishlist = CatalogItem.objects.all()
+    context = {
+            'name' : 'Azzuhri',
+            'npm' : '2006489634',
+            'list_item' : data_wishlist
+    }
+    return render(request, 'katalog.html',context)
